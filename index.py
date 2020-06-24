6c364c61b3506b03f7f85b96c6f64554a0a35447
Mau025 <mmoyapalli@gmail.com>
2020-06-24 15:39:27 -0400

    Parte final del curso de Tkinter, en el cual se completo el proyecto de crud con productos

--- index.py
+++ index.py
@@ -39,7 +39,7 @@ class Product:
         self.tree.heading('#1',text = 'Price', anchor =CENTER)
         #botones
         ttk.Button(text = 'DELETE', command = self.delete_product).grid(row = 5,column = 0,sticky = W + E)
-        ttk.Button(text = 'EDIT').grid(row = 5, column = 1, sticky = W + E)
+        ttk.Button(text = 'EDIT',command = self.edit_product).grid(row = 5, column = 1, sticky = W + E)
 
 
         self.get_product()
@@ -92,6 +92,47 @@ class Product:
         self.run_query(query,(name,))
         self.message['text'] = 'Record {} deleted Successfully'.format(name)
         self.get_product()
+
+    def edit_product(self):
+        self.message['text'] = ''
+        try:
+            self.tree.item(self.tree.selection())['text'][0]
+        except IndexError as e:
+            self.message['text'] = 'please select a record'
+            return
+        self.message['text'] = ''
+
+        name = self.tree.item(self.tree.selection())['text']
+        old_price = self.tree.item(self.tree.selection())['values'][0]
+
+        self.edit_wind = Toplevel()
+        self.edit_wind.title = 'Edit product'
+
+        #datos antiguos
+        Label(self.edit_wind, text ='Old Name: ' ).grid(row = 0, column = 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = name),state = 'readonly').grid(row = 0 , column = 2)
+        #datos nuevos
+        Label(self.edit_wind,text = 'New Name').grid(row=1,column = 1)
+        new_name = Entry(self.edit_wind)
+        new_name.grid(row = 1, column = 2)
+        #precio nuevo
+        Label(self.edit_wind, text = 'Old Price: ').grid(row = 2,column = 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = old_price),state = 'readonly').grid(row = 2,column = 2)
+        #nuevo precio
+        Label(self.edit_wind, text = 'New Price: ').grid(row = 3, column = 1)
+        new_price = Entry(self.edit_wind)
+        new_price.grid(row = 3,column = 2)
+        #boton
+        Button(self.edit_wind,text = 'Update',command = lambda: self.edit_records(new_name.get(),new_price.get(),name,old_price)).grid(row = 4, column =2 , sticky = W)
+
+    def edit_records(self,new_name,new_price,name,old_price):
+        query = 'UPDATE product SET name = ?, price = ? WHERE name = ? AND price = ?'
+        parameters = (new_name,new_price,name,old_price)
+        self.run_query(query,parameters)
+        self.edit_wind.destroy()
+        self.message['text'] = 'Record {} updated Success'.format(name)
+        self.get_product()
+
 
 
 
